b8df3f475e2cc778168f9d43d9edc49f8e331c17
nicatrustemov-12 <nicatrustemov1210@gmail.com>
2026-03-29 22:59:57 +0400

    Update t1.py

--- t1.py
+++ t1.py
@@ -433,8 +433,20 @@ n=int(input())
 print(t(n))'''              #L5,17
 
 
+"""n = 5
+for i in range(1, n+1):
+    print(" "*(n-i), end="")
+    for j in range(1, i+1):
+        print(j,end="")
+    print()"""
 
 
+"""n = 5
+for i in range(1, n+1):
+    print(" "*(n-i), end="")
+    for j in range(1, i+1):
+        print(j,end=" ")
+    print()"""
 
 
 
