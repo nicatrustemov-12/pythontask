8f31aa6775deb340ed92a0252e24a18e27686a61
nicatrustemov-12 <nicatrustemov1210@gmail.com>
2026-05-07 00:44:54 +0400

    Update t1.py

--- t1.py
+++ t1.py
@@ -1033,3 +1033,16 @@ for i in range(len(tek)):
     parol+=tek[i]
     parol+=cut[i]
 print(parol)"""   #10
+
+
+"""n = 5
+
+for i in range(n):
+    print(" "*(n-i), end="")   # boşluq
+
+    val = 1
+    for j in range(i+1):
+        print(val, end=" ")
+        val = val * (i - j) // (j + 1)
+    
+    print()"""  #paskal
